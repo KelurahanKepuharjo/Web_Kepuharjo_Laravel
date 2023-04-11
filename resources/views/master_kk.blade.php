8a2b5a1ca16d5f93496362742f9191b54581c4fa
EdyAtthoillah123 <112244332+EdyAtthoillah123@users.noreply.github.com>
2023-04-11 20:02:02 +0700

    form

--- resources/views/master_kk.blade.php
+++ resources/views/master_kk.blade.php
@@ -125,15 +125,17 @@
                     <form action="{{ url('simpanuser') }}" method="post">
                         @csrf
                         <div class="modal-body">
-                            <div class="form-group">
-                                <label>No Kartu Keluarga</label>
-                                <input type="number" id="input" name="nokk" class="form-control" maxlength="50"
-                                    required="" autocomplete="off" readonly>
-                            </div>
-                            <div class="form-group">
-                                <label>Nama Kepala Keluarga</label>
-                                <input type="text" id="input2" name="kepala_keluarga" class="form-control"
-                                    maxlength="50" required="" autocomplete="off" disabled>
+                            <div class="row form-group">
+                                <div class="col">
+                                    <label>No Kartu Keluarga</label>
+                                    <input type="number" id="input" name="nokk" class="form-control" maxlength="50"
+                                        required="" autocomplete="off" readonly>
+                                </div>
+                                <div class="col">
+                                    <label>Nama Kepala Keluarga</label>
+                                    <input type="text" id="input2" name="kepala_keluarga" class="form-control"
+                                        maxlength="50" required="" autocomplete="off" disabled>
+                                </div>
                             </div>
                             <div class="form-group">
                                 <label>NIK</label>
@@ -156,48 +158,52 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label>Tempat Lahir</label>
-                                <input type="text" name="tempat_lahir" class="form-control" value=""
-                                    maxlength="50" required="" placeholder="Tempat Lahir" autocomplete="off">
-                            </div>
-                            <div class="form-group">
-                                <label>Tanggal Lahir</label>
-                                <input type="date" name="tgl_lahir" class="form-control" value=""
-                                    maxlength="50" required="" placeholder="Tanggal Lahir" autocomplete="off">
-                            </div>
-                            <div class="form-group">
-                                <label>Agama</label>
+                            <div class="row form-group">
                                 <div class="col">
-                                    <select class="form-control" name="agama" autocomplete="off"
-                                        id="exampleFormControlSelect1">
-                                        <option>Pilih</option>
-                                        <option>Islam</option>
-                                        <option>Kristen Protestan</option>
-                                        <option>Katolik</option>
-                                        <option>Hindu</option>
-                                        <option>Buddha</option>
-                                        <option>Konghucu</option>
-                                    </select>
+                                    <label>Tempat Lahir</label>
+                                    <input type="text" name="tempat_lahir" class="form-control" value=""
+                                        maxlength="50" required="" placeholder="Tempat Lahir" autocomplete="off">
+                                </div>
+                                <div class="col">
+                                    <label>Tanggal Lahir</label>
+                                    <input type="date" name="tgl_lahir" class="form-control" value=""
+                                        maxlength="50" required="" placeholder="Tanggal Lahir" autocomplete="off">
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label>Pendidikan</label>
+                            <div class="row form-group">
                                 <div class="col">
-                                    <select class="form-control" name="pendidikan" autocomplete="off"
-                                        id="exampleFormControlSelect1">
-                                        <option>Pilih</option>
-                                        <option>Tidak/Belum Sekolah</option>
-                                        <option>Belum Tamat SD/Sederajat</option>
-                                        <option>Tamat SD/Sederajat</option>
-                                        <option>SLTP/Sederajat</option>
-                                        <option>SLTA/Sederajat</option>
-                                        <option>Diploma I/II</option>
-                                        <option>Diploma III/S.Muda</option>
-                                        <option>Diploma IV/Strata I</option>
-                                        <option>Strata II</option>
-                                        <option>Strata III</option>
-                                    </select>
+                                    <label>Agama</label>
+                                    <div class="col">
+                                        <select class="form-control" name="agama" autocomplete="off"
+                                            id="exampleFormControlSelect1">
+                                            <option>Pilih</option>
+                                            <option>Islam</option>
+                                            <option>Kristen Protestan</option>
+                                            <option>Katolik</option>
+                                            <option>Hindu</option>
+                                            <option>Buddha</option>
+                                            <option>Konghucu</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <label>Pendidikan</label>
+                                    <div class="col">
+                                        <select class="form-control" name="pendidikan" autocomplete="off"
+                                            id="exampleFormControlSelect1">
+                                            <option>Pilih</option>
+                                            <option>Tidak/Belum Sekolah</option>
+                                            <option>Belum Tamat SD/Sederajat</option>
+                                            <option>Tamat SD/Sederajat</option>
+                                            <option>SLTP/Sederajat</option>
+                                            <option>SLTA/Sederajat</option>
+                                            <option>Diploma I/II</option>
+                                            <option>Diploma III/S.Muda</option>
+                                            <option>Diploma IV/Strata I</option>
+                                            <option>Strata II</option>
+                                            <option>Strata III</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-group">
@@ -205,29 +211,31 @@
                                 <input type="text" name="pekerjaan" class="form-control" value=""
                                     maxlength="50" required="" placeholder="Pekerjaan" autocomplete="off">
                             </div>
-                            <div class="form-group">
-                                <label>Golongan Darah</label>
+                            <div class="row form-group">
                                 <div class="col">
-                                    <select class="form-control" name="gol_darah" autocomplete="off"
-                                        id="exampleFormControlSelect1">
-                                        <option>Pilih</option>
-                                        <option>A</option>
-                                        <option>B</option>
-                                        <option>O</option>
-                                        <option>AB</option>
-                                    </select>
+                                    <label>Golongan Darah</label>
+                                    <div class="col">
+                                        <select class="form-control" name="gol_darah" autocomplete="off"
+                                            id="exampleFormControlSelect1">
+                                            <option>Pilih</option>
+                                            <option>A</option>
+                                            <option>B</option>
+                                            <option>O</option>
+                                            <option>AB</option>
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <label>Status Perkawinan</label>
                                 <div class="col">
-                                    <select class="form-control" name="status_perkawinan" autocomplete="off"
-                                        id="exampleFormControlSelect1">
-                                        <option>Pilih</option>
-                                        <option>Belum Kawin</option>
-                                        <option>Kawin</option>
-                                        <option>Cerai</option>
-                                    </select>
+                                    <label>Status Perkawinan</label>
+                                    <div class="col">
+                                        <select class="form-control" name="status_perkawinan" autocomplete="off"
+                                            id="exampleFormControlSelect1">
+                                            <option>Pilih</option>
+                                            <option>Belum Kawin</option>
+                                            <option>Kawin</option>
+                                            <option>Cerai</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-group">
@@ -235,41 +243,55 @@
                                 <input type="date" name="tgl_perkawinan" class="form-control" value=""
                                     maxlength="50" required="" placeholder="Tanggal Perkawinan" autocomplete="off">
                             </div>
-                            <div class="form-group">
-                                <label>Status Keluarga</label>
-                                <input type="text" name="status_keluarga" class="form-control" value=""
-                                    maxlength="50" required="" placeholder="Status Keluarga" autocomplete="off">
-                            </div>
-                            <div class="form-group">
-                                <label>Kewarganegaraan</label>
+                            <div class="row form-group">
                                 <div class="col">
-                                    <select class="form-control" name="kewarganegaraan" autocomplete="off"
-                                        id="exampleFormControlSelect1">
-                                        <option>Pilih</option>
-                                        <option>WNI</option>
-                                        <option>WNA</option>
-                                    </select>
+                                    <label>Status Keluarga</label>
+                                    <div class="col">
+                                        <select class="form-control" name="status_keluarga" autocomplete="off"
+                                            id="exampleFormControlSelect1">
+                                            <option>Pilih</option>
+                                            <option>Kepala Keluarga</option>
+                                            <option>Istri</option>
+                                            <option>Anak</option>
+                                            <option>Wali</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <label>Kewarganegaraan</label>
+                                    <div class="col">
+                                        <select class="form-control" name="kewarganegaraan" autocomplete="off"
+                                            id="exampleFormControlSelect1">
+                                            <option>Pilih</option>
+                                            <option>WNI</option>
+                                            <option>WNA</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label>No Paspor</label>
-                                <input type="text" name="no_paspor" class="form-control" value=""
-                                    maxlength="50" required="" placeholder="No Paspor" autocomplete="off">
+                            <div class="row form-group">
+                                <div class="col">
+                                    <label>No Paspor</label>
+                                    <input type="text" name="no_paspor" class="form-control" value=""
+                                        maxlength="50" required="" placeholder="No Paspor" autocomplete="off">
+                                </div>
+                                <div class="col">
+                                    <label>No KITAP</label>
+                                    <input type="text" name="no_kitap" class="form-control" value=""
+                                        maxlength="50" required="" placeholder="No KITAP" autocomplete="off">
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label>No KITAP</label>
-                                <input type="text" name="no_kitap" class="form-control" value=""
-                                    maxlength="50" required="" placeholder="No KITAP" autocomplete="off">
-                            </div>
-                            <div class="form-group">
-                                <label>Nama Ayah</label>
-                                <input type="text" name="nama_ayah" class="form-control" value=""
-                                    maxlength="50" required="" placeholder="Nama Ayah" autocomplete="off">
-                            </div>
-                            <div class="form-group">
-                                <label>Nama Ibu</label>
-                                <input type="text" name="nama_ibu" class="form-control" value=""
-                                    maxlength="50" required="" placeholder="Nama Ibu" autocomplete="off">
+                            <div class="row form-group">
+                                <div class="col">
+                                    <label>Nama Ayah</label>
+                                    <input type="text" name="nama_ayah" class="form-control" value=""
+                                        maxlength="50" required="" placeholder="Nama Ayah" autocomplete="off">
+                                </div>
+                                <div class="col">
+                                    <label>Nama Ibu</label>
+                                    <input type="text" name="nama_ibu" class="form-control" value=""
+                                        maxlength="50" required="" placeholder="Nama Ibu" autocomplete="off">
+                                </div>
                             </div>
                         </div>
                         <div class="modal-footer">
