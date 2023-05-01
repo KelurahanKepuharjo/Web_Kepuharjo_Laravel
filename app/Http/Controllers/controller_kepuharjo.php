3c24223da78cf6038fee8f3139e45dc2118641a8
Nadia Ayu Safitri <nadiaayus31@gmail.com>
2023-05-01 15:53:08 +0700

    validate mas

--- app/Http/Controllers/controller_kepuharjo.php
+++ app/Http/Controllers/controller_kepuharjo.php
@@ -72,6 +72,37 @@ class controller_kepuharjo extends Controller
     {
         $this->validate($request, [
         ]);
+        $request->validate([
+            'nik' => 'required|max:16|min:16',
+            'nama_lengkap' => 'required',
+            'kelamin' => 'required',
+            'tempat_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'agama' => 'required',
+            'pendidikan' => 'required',
+            'pekerjaan' =>'required',
+            'status_perkawinan' => 'required',
+            'status_keluarga' => 'required',
+            'kewarganegaraan' => 'required',
+            'nama_ayah' => 'required',
+            'nama_ibu' => 'required'
+        ],[
+            'nik.required' => 'NIK Tidak Boleh Kosong',
+            'nik.max' => 'NIK Maksimal 16 Angka',
+            'nik.min' => 'NIK Minimal 16 Angka',
+            'nama_lengkap.required' => 'Nama Lengkap Tidak Boleh Kosong',
+            'kelamin.required' => 'Jenis Kelamin Tidak Boleh Kosong',
+            'tempat_lahir.required'=> 'Tempat Lahir Tidak Boleh Kosong',
+            'tgl_lahir.required' => 'Tanggal Lahir Tidak Boleh Kosong',
+            'agama.required' => 'Agama Tidak Boleh Kosong',
+            'pendidikan.required' => 'Pendidikan Tidak Boleh Kosong',
+            'pekerjaan.required' => 'Pekerjaan Tidak Boleh Kosong',
+            'status_perkawinan.required' => 'Status Perkawinan Tidak Boleh Kosong',
+            'status_keluarga.required' => 'Status Keluarga Tidak Boleh Kosong',
+            'kewarganegaraan.required' => 'Kewarganegaraan Tidak Boleh Kosong',
+            'nama_ayah.required' => 'Nama Ayah Tidak Boleh Kosong',
+            'nama_ibu.required' => 'Nama Ibu Tidak Boleh Kosong'
+        ]);
             try {
                 $data = new master_masyarakat();
                 $uuid = Str::uuid()->toString();
